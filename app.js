8410750672950a5c4390db4e81e783309e32186c
k.kundra2012@gmail.com <Gitkaran!234>
2022-02-23 22:30:35 -0500

    Initial commit

--- app.js
+++ app.js
@@ -112,6 +112,8 @@ app.get("/contact_us", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
